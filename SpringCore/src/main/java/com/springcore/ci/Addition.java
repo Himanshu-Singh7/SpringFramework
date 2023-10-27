9683febf53039640d873b4fdcd34c9560252f0ec
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-27 19:23:38 +0530

    Update Addition.java

--- SpringCore/src/main/java/com/springcore/ci/Addition.java
+++ SpringCore/src/main/java/com/springcore/ci/Addition.java
@@ -8,14 +8,13 @@ public class Addition {
 	public Addition(double a, double b) {
 		this.a = (int) a;
 		this.b = (int) b;
-
-		System.out.println("Consturctor : double ,double");
+               System.out.println("Consturctor : double ,double");
 	}
 
 	public Addition(int a, int b) {
 		this.a = a;
 		this.b = b;
-        System.out.println("Consturctor : int , int");
+                System.out.println("Consturctor : int , int");
 	}
 
 	public Addition(String a,String b) {
