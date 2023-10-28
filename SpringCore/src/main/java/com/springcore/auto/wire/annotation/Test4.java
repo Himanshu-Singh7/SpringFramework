59cf2f7d465629f6278a51c460b35a0e1c3a1493
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-28 19:21:16 +0530

    com/springcore/auto/wire/annotation/
    Practice @Autowire Annotation

---
+++ SpringCore/src/main/java/com/springcore/auto/wire/annotation/Test4.java
@@ -0,0 +1,14 @@
+package com.springcore.auto.wire.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[] args) {
+	ApplicationContext context =	new ClassPathXmlApplicationContext("com/springcore/auto/wire/annotation/autoconfig.xml");
+	Emp emp1 = context.getBean("Emp1",Emp.class);
+	System.out.println(emp1);
+	
+  }
+}
+
