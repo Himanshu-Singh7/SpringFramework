8705e6418fc8c66f2fc212dc3ad249528ed3234b
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-29 17:24:14 +0530

    Update Test6.java

--- SpringCore/src/main/java/com/springcore/stereotype/Test6.java
+++ SpringCore/src/main/java/com/springcore/stereotype/Test6.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test6 {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereotype/steroconfig.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereotype/steroconfig.xml");
         Student student = context.getBean("ob",Student.class);
         System.out.println(student);
         System.out.println(student.getAddress());
